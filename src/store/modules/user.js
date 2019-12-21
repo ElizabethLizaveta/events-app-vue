337b4dc62675283f04a702ca0598909cbf63ab15
Elizabeth Malygina <elizabeth.malygina@gmail.com>
2019-12-21 21:03:50 +0200

    app commit

---
+++ src/store/modules/user.js
@@ -0,0 +1,3 @@
+export const state = {
+  user: { id: 'abc123', name: 'Elisabeth', image: '@/src/assets/IMG_3008.PNG' }
+}
---
+++ src/views/EventList.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <div class="user-row">
+    <h1>Events for {{ user.user.name }}</h1>
+    <img class="user-photo" src="../assets/userphoto.png" alt="user">
+    </div>
+    <EventCard v-for="event in event.events" :key="event.id" :event="event"/>
+    <template v-if="page != 1">
+      <router-link :to="{ name: 'event-list', query: { page: page - 1 } }" rel="prev">
+      Prev Page</router-link>
+      <template v-if="hasNextPage"> | </template>
+    </template>
+    <router-link v-if="hasNextPage" :to="{ name: 'event-list', query: { page: page + 1 } }" rel="next">
+      Next Page</router-link>
+  </div>
+</template>
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+import { mapState } from 'vuex'
+import store from '@/store/store'
+
+function getPageEvents(routeTo, next) {
+  const currentPage = parseInt(routeTo.query.page) || 1
+  store
+    .dispatch('event/fetchEvents', {
+      page: currentPage
+    })
+    .then(() => {
+      routeTo.params.page = currentPage
+      next()
+    })
+}
+
+export default {
+  props: {
+    page: {
+      type: Number,
+      required: true
+    }
+  },
+  components: {
+    EventCard
+  },
+  beforeRouteEnter(routeTo, routeFrom, next) {
+    getPageEvents(routeTo, next)
+  },
+  beforeRouteUpdate(routeTo, routeFrom, next) {
+    getPageEvents(routeTo, next)
+  },
+  computed: {
+    hasNextPage() {
+      return this.event.eventsTotal > this.page * this.event.perPage
+    },
+    ...mapState(['event', 'user'])
+  }
+}
+</script>
+
+<style scoped>
+.user-photo {
+  border-radius: 50% !important;
+  display: block;
+  width: 150px;
+}
+.user-row {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
